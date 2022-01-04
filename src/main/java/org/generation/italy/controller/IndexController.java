cff530b318fc9cf816b082a0777c17b363147ade
Sergio Minissale <minissale.sergio@outlook.itm>
2022-01-04 15:54:07 +0100

    sistemato nav

--- src/main/java/org/generation/italy/controller/IndexController.java
+++ src/main/java/org/generation/italy/controller/IndexController.java
@@ -18,18 +18,20 @@ public class IndexController {
 		model.addAttribute("author", "Sergio");
 		return "index";
 	}
-	
+
 	@GetMapping("/movies")
 	public String movies(Model model) {
 		model.addAttribute("title", "Best Movies of 2021");
-		model.addAttribute("list", Arrays.asList("My Salinger Year", "Palmer", "Blu Bayou", "Cruella", "Percy VS Goliath"));
+		model.addAttribute("list",
+				Arrays.asList("My Salinger Year", "Palmer", "Blu Bayou", "Cruella", "Percy VS Goliath"));
 		return "list";
 	}
-	
+
 	@GetMapping("/songs")
 	public String songs(Model model) {
 		model.addAttribute("title", "Best Songs of 2021");
-		model.addAttribute("list", Arrays.asList("2010 — Earl Sweatshirt", "Keep An Eye on Dan — ABBA","Like I Used To — Sharon Van Etten & Angel Olsen"));
+		model.addAttribute("list", Arrays.asList("2010 — Earl Sweatshirt", "Keep An Eye on Dan — ABBA",
+				"Like I Used To — Sharon Van Etten & Angel Olsen"));
 		return "list";
 	}
 }
